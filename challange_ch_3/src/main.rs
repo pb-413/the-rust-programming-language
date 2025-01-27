50d40ce97bc508b7d19e0761e2a4993f6097cb7f
Patrick K. Byers <pbyers413@gmail.com>
2025-01-26 22:38:32 -0500

    ch 3 challenge 1 convert to celsius

---
+++ challange_ch_3/src/main.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+fn convert_to_celsius(fahrenheit: f32) -> f32 {
+    (fahrenheit - 32.0) * 5.0 / 9.0
+}
+
+fn test_convert_to_celsius() {
+    let mut temps: HashMap<String, f32> = HashMap::new();
+    temps.insert("hot".to_string(), 99.0);
+    temps.insert("warm".to_string(), 72.0);
+    temps.insert("cold".to_string(), 35.0);
+    temps.insert("freezing".to_string(), 32.0);
+    temps.insert("bitter_cold".to_string(), 0.0);
+
+    for (label, temp) in temps {
+        let c = convert_to_celsius(temp);
+        println!("{label}: {temp}F {c}C");
+    }
+    // Surprised that the HashMap was unordered compared to the inserts.
+    // Oh, not only is it un-ordered,
+    // it is randomly ordered when running again.
+}
+
+fn main() {
+    test_convert_to_celsius();
+}
