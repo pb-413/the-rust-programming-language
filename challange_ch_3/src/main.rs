7b1b19a28895e71fa092357b8739177ff6a7ff03
Patrick K. Byers <pbyers413@gmail.com>
2025-01-27 22:26:00 -0500

    ch 3 challenge 3 twelve days of christmas

--- challange_ch_3/src/main.rs
+++ challange_ch_3/src/main.rs
@@ -47,12 +47,42 @@ fn test_fibonacci() {
     }
 }
 
-// TODO: Print the lyrics to the Christmas carol
+// Print the lyrics to the Christmas carol
 // “The Twelve Days of Christmas,”
 // taking advantage of the repetition in the song.
+fn test_twelve_days_of_christmas() {
+    let d1 = "a partridge in a pair tree";
+    let d2 = "two";
+    let d3 = "three";
+    let d4 = "four";
+    let d5 = "five golden rings";
+    let d6 = "six";
+    let d7 = "seven";
+    let d8 = "eight";
+    let d9 = "nine";
+    let d10 = "ten";
+    let d11 = "eleven pipers piping";
+    let d12 = "twelve drummers drumming";
+    let days = [
+        d1, d2, d3,  d4, d5,  d6,
+        d7, d8, d9, d10, d11, d12
+    ];
 
+    for i in 0..12 {
+        let day = i + 1;
+        println!("on the {day} day of christmas");
+        for d in days {
+            println!("{d}");
+            if days[i] == d {
+                break
+            }
+        }
+        println!();
+    }
+}
 
 fn main() {
     // test_convert_to_celsius();
-    test_fibonacci();
+    // test_fibonacci();
+    test_twelve_days_of_christmas();
 }
