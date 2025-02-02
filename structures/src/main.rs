8439265d36017ea3f52684450e4b8ee19f8684c7
Patrick K. Byers <pbyers413@gmail.com>
2025-02-01 20:51:00 -0500

    5.3 method syntax (object oriented programming)
    complete ch 5

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -64,6 +64,59 @@ fn rect_struct_and_area_func() {
 
 }
 
+
+fn rect_struct_and_area_method() {
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    impl Rectangle {
+        fn area(&self) -> u32 {
+            self.width * self.height
+        }
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+        fn square(size: u32) -> Self {
+            Self {
+                width : size,
+                height : size
+            }
+        }
+    }
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let times_table_square = Rectangle::square(12);
+    println!("sqare of {} by {}",
+             times_table_square.width,
+             times_table_square.height);
+}
+
 fn main() {
-    rect_struct_and_area_func();
+    // rect_struct_and_area_func();
+    rect_struct_and_area_method();
+
 }
